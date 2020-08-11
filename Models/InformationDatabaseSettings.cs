ed7dc5f864651b2de0d11fb504168cd9396de075
izzudin26 <azidarmor26@gmail.com>
2020-08-12 01:47:44 +0700

    first commit

---
+++ Models/InformationDatabaseSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mongonets.Models
+{
+    public class InformationDatabaseSettings
+    {
+        public string ColllectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+
+    }
+}
---
+++ Services/PersonServices.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using mongonets.Models;
+using MongoDB.Driver;
+
+namespace mongonets.Services
+{
+    public class PersonServices
+    {
+        private readonly IMongoCollection<Information> _person;
+
+        public PersonServices(InformationDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+            _person = database.GetCollection<Information>(settings.ColllectionName);
+        }
+    }
+}
